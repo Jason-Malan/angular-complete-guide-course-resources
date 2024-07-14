2895790b50a455dd823ba8301d53fb66777f61b5
Jason Malan <jason.malan@digital2dna.com>
2024-07-14 13:50:09 +0200

    Registering all inputs on signup form.

--- code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/signup/signup.component.ts
+++ code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/signup/signup.component.ts
@@ -21,13 +21,34 @@ export class SignupComponent {
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6)],
     }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    street: new FormControl('', { validators: [Validators.required] }),
+    number: new FormControl('', { validators: [Validators.required] }),
+    postalCode: new FormControl('', { validators: [Validators.required] }),
+    city: new FormControl('', { validators: [Validators.required] }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
   onSubmit() {
+    if (this.form.invalid) this.markAll();
     console.log(this.form);
   }
 
   onReset() {
     this.form.reset();
+  }
+
+  markAll() {
+    Object.values(this.form.controls).forEach((control) => {
+      control.markAsDirty();
+      control.markAsTouched();
+    });
   }
 }
