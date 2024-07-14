70507b20430648358ebf9324d493915d6b394793
Jason Malan <jason.malan@digital2dna.com>
2024-07-14 07:53:25 +0200

    Got login reactive form working with routing.

--- code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/login-reactive/login-reactive.component.ts
+++ code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/login-reactive/login-reactive.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-reactive',
   standalone: true,
   templateUrl: './login-reactive.component.html',
   styleUrl: './login-reactive.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class LoginReactiveComponent {}
+export class LoginReactiveComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.form);
+  }
+}
--- code-snapshots/13-forms/001-starting-project - Copy/src/main.ts
+++ code-snapshots/13-forms/001-starting-project - Copy/src/main.ts
@@ -1,15 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  ApplicationConfig,
+  bootstrapApplication,
+} from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
 import { LoginComponent } from './app/auth/login/login.component';
 import { LoginReactiveComponent } from './app/auth/login-reactive/login-reactive.component';
+import { provideZoneChangeDetection } from '@angular/core';
 
 const routes: Routes = [
   { path: 'login-template', component: LoginComponent },
   { path: 'login-reactive', component: LoginReactiveComponent },
 ];
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
-}).catch((err) => console.error(err));
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
