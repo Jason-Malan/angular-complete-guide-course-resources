1cbc9623112f011b913904b674246ae4adfb2bff
Jason Malan <jason.malan@digital2dna.com>
2024-07-14 08:18:13 +0200

    Adding validators.

--- code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/login-reactive/login-reactive.component.ts
+++ code-snapshots/13-forms/001-starting-project - Copy/src/app/auth/login-reactive/login-reactive.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-reactive',
@@ -10,9 +15,29 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginReactiveComponent {
   form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
   });
+
+  get isEmailInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+  get isPasswordInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
 
   onSubmit() {
     console.log(this.form);
