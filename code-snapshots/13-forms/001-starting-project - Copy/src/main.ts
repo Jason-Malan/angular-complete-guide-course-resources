aaffe3bd9489e71fd72c69954f79ea76bccda590
Jason Malan <jason.malan@digital2dna.com>
2024-07-14 13:37:26 +0200

    Making signup component recative forms.

--- code-snapshots/13-forms/001-starting-project - Copy/src/main.ts
+++ code-snapshots/13-forms/001-starting-project - Copy/src/main.ts
@@ -1,17 +1,15 @@
-import {
-  ApplicationConfig,
-  bootstrapApplication,
-} from '@angular/platform-browser';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
 import { LoginComponent } from './app/auth/login/login.component';
 import { LoginReactiveComponent } from './app/auth/login-reactive/login-reactive.component';
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, ApplicationConfig } from '@angular/core';
+import { SignupComponent } from './app/auth/signup/signup.component';
 
 const routes: Routes = [
   { path: 'login-template', component: LoginComponent },
   { path: 'login-reactive', component: LoginReactiveComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
